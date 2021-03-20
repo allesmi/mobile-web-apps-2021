0e06e373e0e591932b331f6cd1906d8204512f66
Alexander Miller <alles.mil@gmail.com>
2021-03-20 15:03:09 +0100

    Add code for session 9

---
+++ src/09/script.js
@@ -0,0 +1,5 @@
+const as = new Adresssuche(document.querySelector('#adresssuche'));
+const as2 = new Adresssuche(document.querySelector('#adresssuche2'));
+
+const coords = as.getCoords();
+console.log(coords);
